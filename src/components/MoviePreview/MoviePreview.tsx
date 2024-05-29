0c8145549f08dc1ed5b0154f8541b4026e81b196
louispny <louis.pigny@insa-lyon.fr>
2024-05-29 20:28:16 +0200

    partie 1

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,26 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({ movie }: { movie: Movie }) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return (
+    <div className={styles.movieCard} onClick={goToMovieDetails}>
+      <img src={POSTER_URL_PREFIX + movie.poster_path}></img>
+      <p className={styles.title}>{movie.title}</p>
+      <div className="stars">
+        {[...Array(5)].map((_, index) => (
+        <span className={`${styles.star} ${
+          ((movie.vote_average/2) <= index) ? styles.gray : styles.yellow
+        }`}>★</span>
+      ))}
+      </div>
+      
+    </div>
+  );
 };
 
 export default MoviePreview;
